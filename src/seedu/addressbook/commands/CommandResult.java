aa750b1ae83047904dfe699d5aae9ad6c4518322
GARYTANG\Tang <e0003506@u.nus.edu>
2016-08-31 12:44:39 +0800

    changed access level of "feedbackToUser" from public to private, added a new public method getFeedbackToUser so that the variable can be accessed from the class itself only.

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    //public final String feedbackToUser;
+	private final String feedbackToUser // Changed variable from public to private
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -33,4 +34,7 @@ public class CommandResult {
         return Optional.ofNullable(relevantPersons);
     }
 
+    // Created new method for access to newly-changed private variable "feedbackToUser"
+    public String getFeedbackToUser(){
+    	return feedbackToUser;
 }
